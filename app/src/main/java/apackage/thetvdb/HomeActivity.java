33eecc7c2b6280c0fa4f4f101250ff9ed807a6dc
Gianni Azizi <gianni.azizi@bnpparibas-pf.com>
2017-11-23 12:01:51 +0100

    Add HomeFragment and SearchFragment

--- app/src/main/java/apackage/thetvdb/HomeActivity.java
+++ app/src/main/java/apackage/thetvdb/HomeActivity.java
@@ -7,6 +7,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
 
+import apackage.thetvdb.fragments.HomeFragment;
+import apackage.thetvdb.fragments.SearchFragment;
+
 public class HomeActivity extends AppCompatActivity {
 
     private TextView mTextMessage;
@@ -18,10 +21,14 @@ public class HomeActivity extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_home:
+                    getSupportFragmentManager().beginTransaction()
+                            .replace(R.id.fragment_container, new HomeFragment())
+                            .commit();
                     return true;
                 case R.id.navigation_search:
-                    return true;
-                case R.id.navigation_notifications:
+                    getSupportFragmentManager().beginTransaction()
+                            .replace(R.id.fragment_container, new SearchFragment())
+                            .commit();
                     return true;
             }
             return false;
@@ -32,9 +39,13 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        
+
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.fragment_container, new HomeFragment())
+                .commit();
     }
 
 }
--- app/src/main/java/apackage/thetvdb/fragments/SearchFragment.java
+++ app/src/main/java/apackage/thetvdb/fragments/SearchFragment.java
@@ -8,17 +8,15 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import apackage.thetvdb.R;
 
 
-public class HomeFragment extends Fragment {
-
+public class SearchFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_home,container,false);
+        return inflater.inflate(R.layout.fragment_search,container,false);
     }
 
     @Override
