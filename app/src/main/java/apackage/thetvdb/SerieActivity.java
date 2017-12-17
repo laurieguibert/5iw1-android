34e2142e9acd9a2b21cfc1e15111e2a6be837a68
Poirier Elise <elisepoirier@outlook.com>
2017-12-17 19:04:52 +0100

    Add global design and template for serie activity

--- app/src/main/java/apackage/thetvdb/SerieActivity.java
+++ app/src/main/java/apackage/thetvdb/SerieActivity.java
@@ -4,12 +4,20 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
 
 import apackage.thetvdb.entity.Serie;
 
 public class SerieActivity extends AppCompatActivity {
 
     private final static String SERIE_KEY = "serie";
+
+    private TextView seriesName;
+    private TextView overview;
+    private ImageView banner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,5 +26,14 @@ public class SerieActivity extends AppCompatActivity {
 
         Intent intent = getIntent();
         Serie serie = intent.getParcelableExtra(SERIE_KEY);
+
+        seriesName = (TextView) findViewById(R.id.series_name);
+        overview = (TextView) findViewById(R.id.overview);
+        banner = (ImageView) findViewById(R.id.banner);
+
+        seriesName.setText(serie.getSeriesName());
+        overview.setText(serie.getOverview());
+        Picasso.with(this).load("https://www.thetvdb.com/banners/_cache/" + serie.getBanner()).into(banner);
+
     }
 }
